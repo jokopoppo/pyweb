d17e1dfdb886f4c67ac348f9f21233cc69842318
winner <wachirawit.kan@gmail.com>
2018-05-30 10:40:14 +0700

    test deploy

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from app.app import *
+from app.app import test
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     return 'Deploy Success'
-#
-# if __name__ == '__main__':
-#     app.run()
+@app.route('/')
+def index():
+    return 'app front'
+
+if __name__ == '__main__':
+    app.run()
